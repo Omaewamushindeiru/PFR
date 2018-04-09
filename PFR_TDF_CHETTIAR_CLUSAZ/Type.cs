604ec930527d73d73cec2720a500a44db22637d7
MaxCLZ <38212501+MaxCLZ@users.noreply.github.com>
2018-04-09 16:12:03 +0200

    Ajout Type

---
+++ PFR_TDF_CHETTIAR_CLUSAZ/Type.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetZombillenium
+{
+    enum TypeSexe
+    {
+        male, femelle, autre
+    }
+    enum TypeCategorie
+    {
+        assise, inversee, bobsleigh
+    }
+    enum TypeBoutique
+    {
+        souvenir, barbeAPape, nourriture
+    }
+    enum Grade
+    {
+        novice, mega, giga, strata
+    }
+    enum CouleurZ
+    {
+        bleuatre, grisatre
+    }
+}
