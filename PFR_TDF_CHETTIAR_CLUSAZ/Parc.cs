2ae7beba83788bf9585dd333dd6e31b6de4e6efd
CLUSAZ Maxence <maxence.clusaz@devinci.fr>
2018-04-09 16:31:47 +0200

    ajout

--- PFR_TDF_CHETTIAR_CLUSAZ/Parc.cs
+++ PFR_TDF_CHETTIAR_CLUSAZ/Parc.cs
@@ -88,5 +88,5 @@ namespace ProjetZombillenium
     }
 
 }
-    }
+    
 }
--- PFR_TDF_CHETTIAR_CLUSAZ/Personnel.cs
+++ PFR_TDF_CHETTIAR_CLUSAZ/Personnel.cs
@@ -4,9 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PFR_TDF_CHETTIAR_CLUSAZ
+namespace ProjetZombillenium
 {
-    class Personnel
+    class Personnel : Parc
     {
+        private string fonction;
+        private int matricule;
+        private string nom;
+        private string prenom;
+        private TypeSexe sexe;
+
+        public Personnel(int matricule, string nom, string prenom, TypeSexe sexe, string fonction)
+        {
+            this.fonction = fonction;
+            this.matricule = matricule;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.sexe = sexe;
+        }
+
     }
+
+
 }
